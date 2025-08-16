b46ca2fdcbad688c33bd2d35870a3cec99e08331
parking_turkeys <p-t@hackclub.app>
2025-08-16 13:02:16 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+# note: if you are using Windows 7/8/8.1, you will need to install the "Universal C Runtime from Microsoft’s website"
+
+from krita import *      # import the krita plugin -- this will work in the krita scripting thing
